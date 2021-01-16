5b1329d46359bcb2a316ff1e7c5d65969ef3b435
Mr. Kutin <serge.kutin@gmail.com>
2021-01-16 16:41:30 +0300

    live update of selected note

--- web-client/src/components/NoteEditor.vue
+++ web-client/src/components/NoteEditor.vue
@@ -1,33 +1,49 @@
 <template>
 
   <b-form>
-      <b-form-input
-          id="input-1"
-          placeholder="Note caption"
-          required
-      ></b-form-input>
-
-      <b-form-textarea
-          id="textarea-rows"
-          placeholder="Put your note here"
-          rows="20"
-      />
+    <b-form-input
+        :value="selectedNote ? selectedNote.caption : ''"
+        placeholder="Put your note caption here"
+        required
+        v-on:keyup="onCaptionType"
+        v-on:change="onChange"
+    ></b-form-input>
+    <b-form-textarea
+        :value="selectedNote ? selectedNote.text : ''"
+        placeholder="Put your note text here"
+        rows="20"
+        v-on:keyup="onTextType"
+        v-on:change="onChange"
+    />
   </b-form>
 </template>
 
 <script>
+
+import {mapGetters} from 'vuex'
+
 export default {
-  name: "NoteEditor"
+  name: "NoteEditor",
+  computed: mapGetters(['selectedNote']),
+  methods: {
+    onCaptionType(e) {
+      this.selectedNote.caption = e.target.value
+    },
+    onTextType(e) {
+      this.selectedNote.text = e.target.value
+    },
+    onChange(){
+      //todo save it to the DB
+    }
+  }
 }
 </script>
 
 <style scoped>
-  input, textarea {
-    border: none;
-    outline: 0 !important;
-    -webkit-appearance: none;
-    box-shadow: none !important;
-  }
-
-
+input, textarea {
+  border: none;
+  outline: 0 !important;
+  -webkit-appearance: none;
+  box-shadow: none !important;
+}
 </style>

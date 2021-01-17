c8de5676da444048bff4ec4a9cef56362cc7bff7
Mr. Kutin <serge.kutin@gmail.com>
2021-01-17 16:36:55 +0300

    new store

--- web-client/src/store.js
+++ web-client/src/store.js
@@ -19,9 +19,9 @@ function generateId(length) {
 
 const state = {
   notes: [
-    {_id: '9797909807980789709', caption: 'First Note', text: 'This is my first note', updatedAt: new Date()},
+    {_id: '0809789675765645646', caption: 'Third Note', text: 'This is my third note', updatedAt: new Date()},
     {_id: '6567698990898876777', caption: 'Second Note', text: 'This is my second note', updatedAt: new Date()},
-    {_id: '0809789675765645646', caption: 'Third Note', text: 'This is my third note', updatedAt: new Date()}
+    {_id: '9797909807980789709', caption: 'First Note', text: 'This is my first note', updatedAt: new Date()}
   ],
   currentNote: null
 }

b6daa2747f46cddbffe0f9dc40365fdcaa6026c3
Mr. Kutin <serge.kutin@gmail.com>
2021-01-18 16:56:36 +0300

    button-toolbar

--- web-client/src/components/NotePreview.vue
+++ web-client/src/components/NotePreview.vue
@@ -1,13 +1,26 @@
 <template>
   <div>
     <b-card
-        v-on:click="onClick"
-        v-on:mouseenter="toggleHighlight"
-        v-on:mouseleave="toggleHighlight"
-        :bg-variant="highlighted ? 'light' : 'default'"
+        v-on:click="onEditClick"
+        v-on:mouseenter="toggleHovered"
+        v-on:mouseleave="toggleHovered"
+        v-on:touchmove="toggleHovered"
+        :bg-variant="hovered ? 'light' : 'default'"
         class="custom-card border-top-0 border-right-0 border-left-0 border-bottom"
     >
-      <b-card-title><h4>{{note.caption}}</h4></b-card-title>
+      <b-list-group-item class="d-flex justify-content-between align-items-center p-0 border-0 bg-transparent">
+        <b-card-title>{{note.caption}}</b-card-title>
+        <b-button-toolbar v-if="hovered">
+          <b-button-group class="mr-1">
+            <b-button title="Edit note" v-on:click.stop="onEditClick">
+              <b-icon icon="file-earmark-text" aria-hidden="true"></b-icon>
+            </b-button>
+            <b-button title="Delete note" v-on:click.stop="onDeleteClick">
+              <b-icon icon="x-circle-fill" aria-hidden="true"></b-icon>
+            </b-button>
+          </b-button-group>
+        </b-button-toolbar>
+      </b-list-group-item>
       <b-card-sub-title class="ml-2">{{new Date(note.updatedAt).toDateString()}}</b-card-sub-title>
       <b-card-text v-html="note.text" class="text-truncate ml-2"/>
     </b-card>
@@ -20,14 +33,17 @@ export default {
   props: ['note'],
   data() {
     return {
-      highlighted: false
+      hovered: false
     }
   },
   methods: {
-    toggleHighlight() {
-      this.highlighted = !this.highlighted
+    onDeleteClick(){
+      alert()
     },
-    onClick() {
+    toggleHovered() {
+      this.hovered = !this.hovered
+    },
+    onEditClick() {
       this.$router.push(`/notes/${this.note._id}`)
     }
   }

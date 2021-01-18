8f0b7d20043c179c43c2a7677f9721db4ddc26a0
Mr. Kutin <serge.kutin@gmail.com>
2021-01-18 16:08:26 +0300

    small tweaks on card

--- web-client/src/components/NotePreview.vue
+++ web-client/src/components/NotePreview.vue
@@ -1,32 +1,33 @@
 <template>
-  <b-card
-      v-on:click="onClick"
-      v-on:mouseenter="toggleHighlight"
-      v-on:mouseleave="toggleHighlight"
-      :title="note.caption"
-      :sub-title="new Date(note.updatedAt).toDateString()"
-      :bg-variant="highlighted ? 'light' : 'default'"
-      class="custom-card border-top-0 border-right-0 border-left-0 border-bottom"
-  >
-    <b-card-text v-html="note.text" class="text-truncate"/>
-  </b-card>
+  <div>
+    <b-card
+        v-on:click="onClick"
+        v-on:mouseenter="toggleHighlight"
+        v-on:mouseleave="toggleHighlight"
+        :bg-variant="highlighted ? 'light' : 'default'"
+        class="custom-card border-top-0 border-right-0 border-left-0 border-bottom"
+    >
+      <b-card-title><h4>{{note.caption}}</h4></b-card-title>
+      <b-card-sub-title class="ml-2">{{new Date(note.updatedAt).toDateString()}}</b-card-sub-title>
+      <b-card-text v-html="note.text" class="text-truncate ml-2"/>
+    </b-card>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: 'NotePreview',
   props: ['note'],
-  data () {
+  data() {
     return {
       highlighted: false
     }
   },
   methods: {
-    toggleHighlight(){
+    toggleHighlight() {
       this.highlighted = !this.highlighted
     },
-    onClick(){
+    onClick() {
       this.$router.push(`/notes/${this.note._id}`)
     }
   }

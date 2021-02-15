17d35f782629505de6c798b75d51d7d10d8a0f5f
Mr. Kutin <serge.kutin@gmail.com>
2021-02-15 10:50:55 +0300

    middleware redirect to login

--- web-client/src/router.js
+++ web-client/src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from './store'
 import NoteListWithEditor from './pages/NoteListWithEditor'
 import EditNote from './pages/EditNote'
 import SignUp from './pages/SignUp'
@@ -9,16 +10,30 @@ import Index from './pages/Index'
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', component: Index },
-  { path: '/notes', component: NoteListWithEditor },
-  { path: '/notes/:id', component: EditNote, props: true },
-  { path: '/signup', component: SignUp },
-  { path: '/login', component: LogIn }
+  {name: 'index', path: '/', component: Index},
+  {name: 'note-list', path: '/notes', component: NoteListWithEditor},
+  {name: 'edit-note', path: '/notes/:id', component: EditNote, props: true},
+  {name: 'signup', path: '/signup', component: SignUp},
+  {name: 'login', path: '/login', component: LogIn}
 ]
+
+const publicPageNames = ['index', 'signup', 'login']
 
 const router = new VueRouter({
   mode: 'history',
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if (publicPageNames.includes(to.name)) {
+    next()
+  } else {
+    if (store.getters.isLoggedIn) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+  })
 
 export default router

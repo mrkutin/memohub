8ca6e79665ad194da5519927711baa1e038a11d7
Mr. Kutin <serge.kutin@gmail.com>
2021-03-02 10:23:40 +0300

    file picker

--- web-client/src/components/NoteToolbar.vue
+++ web-client/src/components/NoteToolbar.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="h3 mt-2" v-if="note">
-    <b-button variant="light" v-on:click="onDeleteClick">
-      <b-icon icon="trash"></b-icon>
+  <div class="h3 mt-2 mr-3"
+       v-on:click="toggleFilePicker"
+       v-if="note"
+  >
+    <b-button variant="light">
+      <b-icon icon="paperclip"></b-icon>
     </b-button>
     <b-button variant="light">
       <b-icon icon="link"></b-icon>
     </b-button>
+    <b-button variant="light" v-on:click="onDeleteClick">
+      <b-icon icon="trash"></b-icon>
+    </b-button>
+
+    <b-form-file v-if="showFilePicker" size="sm"/>
   </div>
 </template>
 
@@ -15,9 +23,17 @@ import {mapActions} from 'vuex'
 export default {
   name: 'NoteToolbar',
   props: ['note'],
+  data () {
+    return {
+      showFilePicker: false
+    }
+  },
   methods: {
     ...mapActions(['deleteNote']),
-    onDeleteClick(){
+    toggleFilePicker() {
+      this.showFilePicker = !this.showFilePicker
+    },
+    onDeleteClick() {
       if (confirm('Are you sure?')) {
         this.deleteNote(this.note)
       }
